dc0fe5cc0c3db572bf3064b5f89aecd338673d2d
Eskil Heyn Olsen <eskil@eskil.org>
2023-05-19 16:42:53 -0700

    Initial Commit

---
+++ DegreeCalculator/DegreeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DegreeCalculatorApp.swift
+//  DegreeCalculator
+//
+//  Created by Eskil Olsen on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DegreeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
