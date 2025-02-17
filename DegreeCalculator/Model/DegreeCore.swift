940c8c5c40184bf8ea05f1077d2dc415dcb6ab65
Eskil Heyn Olsen <eskil@eskil.org>
2025-02-16 23:58:02 -0800

    Add some docstrings/explations to ModelData and DegreeCore

--- DegreeCalculator/Model/DegreeCore.swift
+++ DegreeCalculator/Model/DegreeCore.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/**
+ Value is the part of an expression that models the numeric value.
+ 
+ It only supports degrees (int) and minutes (decimal).
+ */
 struct Value: Codable, Hashable, CustomStringConvertible {
     var degrees: Int
     var minutes: Decimal
@@ -21,7 +26,9 @@ struct Value: Codable, Hashable, CustomStringConvertible {
         self.minutes = minutes
     }
     
-    // Format to <degrees>°<mm>'<s> format.
+    /**
+     Format to  displayable `<degrees>°<mm>'<s>` format used inside the calculator view.
+     */
     public var description: String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
@@ -39,10 +46,16 @@ struct Value: Codable, Hashable, CustomStringConvertible {
     }
 }
 
+/**
+ The Operator is the part of the expression that models the operator.
+ */
 enum Operator: String, CustomStringConvertible, Hashable, Codable {
     case Add
     case Subtract
     
+    /**
+     Format to  displayable `string used inside the calculator view.
+     */
     public var description: String {
         switch self {
         case .Add:
@@ -53,6 +66,17 @@ enum Operator: String, CustomStringConvertible, Hashable, Codable {
     }
 }
 
+/**
+ Expr captures the binary tree structure of an expression.
+ It has an Operator and array of Exprs the operator applies to.
+ The operator is optional and it can be a value instead.
+ 
+ This is binary tree, so the array of Exprs is at max two, representing
+ left and right.
+ 
+ So eg. a basic 2+3 is an Expr with op = PLUS and two Exprs,
+ each that have no op but have a value.
+ */
 struct Expr: CustomStringConvertible, Hashable, Codable {
     static func == (lhs: Expr, rhs: Expr) -> Bool {
         return lhs.value == rhs.value
@@ -62,58 +86,6 @@ struct Expr: CustomStringConvertible, Hashable, Codable {
     var op: Operator?
     var v: Value?
     
-    var value: Value? {
-        get {
-            if v != nil {
-                return v
-            }
-            if nodes.count == 0 {
-                return nil
-            }
-            
-            if op == nil || nodes.count == 1 {
-                return nil
-            }
-
-            if let lv = nodes[0].value, let rv = nodes[1].value {
-                var degrees: Int = 0
-                var minutes: Decimal = 0.0
-                
-                switch op! {
-                case Operator.Add:
-                    degrees = lv.degrees + rv.degrees
-                    minutes = lv.minutes + rv.minutes
-                case Operator.Subtract:
-                    degrees = lv.degrees - rv.degrees
-                    minutes = lv.minutes - rv.minutes
-                }
-                
-                // This could be done via % 60 and % 360 and checking
-                // for negative. But this is not runtime sensitive and
-                // this extremely literal version is an easy to
-                // understand/read reflection of how a person does
-                // this math.
-                while minutes >= 60.0 {
-                    degrees += 1
-                    minutes -= 60.0
-                }
-                while minutes < 0.0  {
-                    minutes += 60.0
-                    degrees -= 1
-                }
-                while degrees >= 360 {
-                    degrees -= 360
-                }
-                while degrees < 0 {
-                    degrees += 360
-                }
-                return Value(degrees: degrees, minutes: minutes)
-            } else {
-                return nodes[0].value
-            }
-        }
-    }
-
     // Expr can be empty, no children and no op
     init() {
         self.op = nil
@@ -146,7 +118,76 @@ struct Expr: CustomStringConvertible, Hashable, Codable {
             self.nodes.append(r)
         }
     }
+    /**
+     value recursively computes the expression value.
+     If the expression is fully formed (has operator, left and right), this function
+     computes the value by applying the operator to the result of calling
+     value on the left and right nodes.
+     
+     There's no caching of the value (for simplicity's sake), but the operations
+     are so simple that this should not pose a problem.
+     */
+    var value: Value? {
+        get {
+            if v != nil {
+                return v
+            }
+            if nodes.count == 0 {
+                return nil
+            }
+            
+            if op == nil || nodes.count == 1 {
+                return nil
+            }
 
+            // Compute left and right side, and if they're
+            // non-nil, compute the result.
+            if let lv = nodes[0].value, let rv = nodes[1].value {
+                var degrees: Int = 0
+                var minutes: Decimal = 0.0
+                
+                // Apply the operation first...
+                switch op! {
+                case Operator.Add:
+                    degrees = lv.degrees + rv.degrees
+                    minutes = lv.minutes + rv.minutes
+                case Operator.Subtract:
+                    degrees = lv.degrees - rv.degrees
+                    minutes = lv.minutes - rv.minutes
+                }
+                
+                // Then normalise to keep inside [0..360],
+                // so eg. -10 = 360-10 and 370 = 370 - 360
+                
+                // This could be done via % 60 and % 360 and checking
+                // for negative. But this is not runtime sensitive and
+                // this extremely literal version is an easy to
+                // understand/read reflection of how a person does
+                // this math.
+                while minutes >= 60.0 {
+                    degrees += 1
+                    minutes -= 60.0
+                }
+                while minutes < 0.0  {
+                    minutes += 60.0
+                    degrees -= 1
+                }
+                while degrees >= 360 {
+                    degrees -= 360
+                }
+                while degrees < 0 {
+                    degrees += 360
+                }
+                return Value(degrees: degrees, minutes: minutes)
+            } else {
+                return nodes[0].value
+            }
+        }
+    }
+    
+    /**
+     Format to  displayable `string used inside the calculator view.
+     */
     public var description: String {
         var result: [String] = []
         if let v = v {
@@ -177,6 +218,9 @@ struct Expr: CustomStringConvertible, Hashable, Codable {
         return result.joined(separator: " ")
     }
     
+    /**
+     Inorder visit the tree, apply visit function to each node.
+     */
     public func inOrder(visit: (Expr) -> Void) {
         if nodes.count > 0 {
             nodes[0].inOrder(visit: visit)
